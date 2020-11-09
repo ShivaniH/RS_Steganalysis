82c769ce44528f51c65145b5c9eca525a0f51fc6
pratikk-bulani <pratikkumar.bulani@students.iiit.ac.in>
2020-11-09 14:20:23 +0530

    initial commit 4

--- src/util.py
+++ src/util.py
@@ -24,9 +24,13 @@ def flipping_operation(np_img_window, np_mask):
 def calculate_count_groups(np_img, np_mask):
     count_reg, count_sing = 0, 0
     for ih in range(0, np_img.shape[0], np_mask.shape[0]):
-        for iw in range(0, np_img.shape[1], np_img.shape[1]):
+        for iw in range(0, np_img.shape[1], np_mask.shape[1]):
             np_img_window = np_img[ih: ih+np_mask.shape[0], iw: iw+np_mask.shape[1]]
             flipped_output = flipping_operation(np_img_window, np_mask)
+            print("Image window")
+            print(np_img_window)
+            print("Flipped version")
+            print(flipped_output)
             
             discrimination_img_window = discrimination_function(np_img_window)
             discrimination_flipped_output = discrimination_function(flipped_output)
