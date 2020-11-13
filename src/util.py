ec969cb02176cc7b7eba74b99aac62e5dc6f553d
pratikk-bulani <pratikkumar.bulani@students.iiit.ac.in>
2020-11-13 11:41:05 +0530

    random scatter 2

--- src/util.py
+++ src/util.py
@@ -2,25 +2,24 @@ import numpy as np
 import random
 import string
 
+# research paper discrimination function
 def discrimination_function(np_img_window):
-
-    if len(np_img_window.shape) == 3:
-        # print("RGB image")
-        np_img_window_0 = np_img_window[:,:,0].flatten()
-        np_img_window_1 = np_img_window[:,:,1].flatten()
-        np_img_window_2 = np_img_window[:,:,2].flatten()
+    if len(np_img_window.shape) == 3: # for RGB
+        np_img_window_0, np_img_window_1, np_img_window_2 = np_img_window[:,:,0].flatten(), np_img_window[:,:,1].flatten(), np_img_window[:,:,2].flatten()
 
         channelSum0 = np.sum(np.abs(np_img_window_0[:-1] - np_img_window_0[1:])) 
         channelSum1 = np.sum(np.abs(np_img_window_1[:-1] - np_img_window_1[1:]))
         channelSum2 = np.sum(np.abs(np_img_window_2[:-1] - np_img_window_2[1:]))
 
         return (channelSum0 + channelSum1 + channelSum2)
-    else:
+    
+    elif len(np_img_window.shape) == 2: # for grayscale
         np_img_window = np_img_window.flatten()
         return np.sum(np.abs(np_img_window[:-1] - np_img_window[1:]))
     
+    else:
+        raise Exception("Error: Invlaid shape of image in discrimination_function")
     # Extra subtractions across channels -- Is this a problem?
-
 
 def support_f_1(np_img_window):
     np_img_window = np.copy(np_img_window)
@@ -28,7 +27,6 @@ def support_f_1(np_img_window):
     np_img_window[even_values] += 1
     np_img_window[np.logical_not(even_values)] -= 1
     return np_img_window
-
 
 def flipping_operation(np_img_window, np_mask):
     f_1 = lambda x: support_f_1(x)
@@ -41,11 +39,8 @@ def flipping_operation(np_img_window, np_mask):
         np_result[temp_indices_x, temp_indices_y] = dict_flip[i](np_img_window[temp_indices_x, temp_indices_y])
     return np_result
 
-
 def calculate_count_groups(np_img, np_mask):
-
-    count_reg, count_sing = 0, 0
-    count_unusable = 0
+    count_reg, count_sing, count_unusable = 0, 0, 0
 
     for ih in range(0, np_img.shape[0], np_mask.shape[0]):
         for iw in range(0, np_img.shape[1], np_mask.shape[1]):
@@ -63,17 +58,22 @@ def calculate_count_groups(np_img, np_mask):
                 count_unusable += 1
 
     totalGroups = count_reg + count_sing + count_unusable
-
-    return ((count_reg/totalGroups), (count_sing/totalGroups))
-
+    return count_reg/totalGroups, count_sing/totalGroups
 
 def randomString(n):
     chars = string.ascii_lowercase
     return ''.join(random.choice(chars) for _ in range(n))
-
 
 def simpleLSBFlipper(image, n):
     flattenedImg = image.flatten()
     flipped = flattenedImg[0:n]^1       # 0 XOR 1 = 1 ; 1 XOR 1 = 0
     flattenedImg[0:n] = flipped
     return flattenedImg.reshape(image.shape)
+
+def scattered_lsb_flipping(np_img, percent):
+    np_result = np.copy(np_img)
+    no_pixels_to_change = int(np.round(percent * np.prod(np_img.shape)))
+    random_indices_c = np.random.randint(low = 0, high = np_img.shape[1], size = no_pixels_to_change)
+    random_indices_r = np.random.randint(low = 0, high = np_img.shape[0], size = no_pixels_to_change)
+    np_result[random_indices_r, random_indices_c] = np.bitwise_xor(np_result[random_indices_r, random_indices_c], 1)
+    return np_result
