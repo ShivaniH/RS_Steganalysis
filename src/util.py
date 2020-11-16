851fb986524b6721658a77914c820e91ad489466
thesigmaguy <dhawal.sirikonda@research.iiit.ac.in>
2020-11-16 16:07:35 +0530

    finding roots of equation

--- src/util.py
+++ src/util.py
@@ -1,172 +1,145 @@
 import numpy as np
 import random
 import string
-import copy
-
-mask = np.array([[0, 1, 0]])
-
-# research paper discrimination function
-
-def discrimination_function(np_img_window):
-    if len(np_img_window.shape) == 3:               # for RGB
-
-        # np_img_window_0, np_img_window_1, np_img_window_2 = np_img_window[:,:,0].flatten(), np_img_window[:,:,1].flatten(), np_img_window[:,:,2].flatten()
-
-        # channelSum0 = np.sum(np.abs(np_img_window_0[:-1] - np_img_window_0[1:])) 
-        # channelSum1 = np.sum(np.abs(np_img_window_1[:-1] - np_img_window_1[1:]))
-        # channelSum2 = np.sum(np.abs(np_img_window_2[:-1] - np_img_window_2[1:]))
-
-        # return (channelSum0 + channelSum1 + channelSum2)
-
-        red_sum = 0
-        blue_sum = 0
-        green_sum = 0
-
-        np_img_window_0 = np_img_window[:, :, 0]
-        np_img_window_1 = np_img_window[:, :, 1]
-        np_img_window_2 = np_img_window[:, :, 2]
-
-        for i in range(np_img_window_0.shape[0]):
-            for j in range(1, np_img_window_0.shape[1]):
-                red_sum += abs(np_img_window_0[i, j] - np_img_window_0[i, j-1])
-        
-        for j in range(np_img_window_0.shape[1]):
-            for i in range(1, np_img_window_0.shape[0]):
-                red_sum += abs(np_img_window_0[i, j] - np_img_window_0[i-1, j])
-
-        for i in range(np_img_window_1.shape[0]):
-            for j in range(1, np_img_window_1.shape[1]):
-                blue_sum += abs(np_img_window_1[i, j] - np_img_window_1[i, j-1])
-        
-        for j in range(np_img_window_1.shape[1]):
-            for i in range(1, np_img_window_1.shape[0]):
-                blue_sum += abs(np_img_window_1[i, j] - np_img_window_1[i-1, j])
-
-        for i in range(np_img_window_2.shape[0]):
-            for j in range(1, np_img_window_2.shape[1]):
-                green_sum += abs(np_img_window_2[i, j] - np_img_window_2[i, j-1])
-        
-        for j in range(np_img_window_2.shape[1]):
-            for i in range(1, np_img_window_2.shape[0]):
-                green_sum += abs(np_img_window_2[i, j] - np_img_window_2[i-1, j])
-
-        sum = red_sum + blue_sum + green_sum
-
-        return sum
-    
-    elif len(np_img_window.shape) == 2:                 # for grayscale
-        np_img_window = np_img_window.flatten()
-        return np.sum(np.abs(np_img_window[:-1] - np_img_window[1:]))
-    
-    else:
-        raise Exception("Error: Invlaid shape of image window in discrimination_function")
 
 
-def support_f_1(np_img_window):
-    np_img_window = np.copy(np_img_window)
-    even_values = np_img_window % 2 == 0
-    np_img_window[even_values] += 1
-    np_img_window[np.logical_not(even_values)] -= 1
-    return np_img_window
+def discrimination_function(img_window: np.array) -> np.array:
+    """
+    :param img_window: np.array of img window
+    :return: discriminated output
+
+    Function: takes a zig-zag scan of img_window then returns
+    Sum of abs(X[i] - X[i-1])
+    """
+    img_window = np.concatenate([
+        np.diagonal(img_window[::-1, :], k)[::(2 * (k % 2) - 1)]
+        for k in range(1 - img_window.shape[0], img_window.shape[0])
+    ])  # zigzag scan
+    return np.sum(np.abs(img_window[:-1] - img_window[1:]))
 
 
-def flipping_operation(np_img_window, np_mask):
-    f_1 = lambda x: support_f_1(x)
-    f_0 = lambda x: x
-    f_neg1 = lambda x: support_f_1(x+1)-1
-    np_result = np.empty(np_img_window.shape)
-    dict_flip = {-1:f_neg1, 0:f_0, 1:f_1}
+def support_f_1(img_window: np.array) -> np.array:
+    """
+    :param img_window: img to window to flip
+    :return: flipped img_window
+    Support for F1 function
+    """
+    img_window = np.copy(img_window)
+    even_values = img_window % 2 == 0
+    img_window[even_values] += 1
+    img_window[np.logical_not(even_values)] -= 1
+    return img_window
+
+
+def flipping_operation(img_window: np.array, mask: np.array) -> np.array:
+    """
+    :param img_window: img window for flipping
+    :param mask: mask using which what flipping to be performed at that location
+    :return: np.array of flipped image based on mask
+    Function performs flipping operation based on the masked passed in as input
+    """
+
+    def f_1(x: np.array) -> np.array: return support_f_1(x)
+
+    def f_0(x: np.array) -> np.array: return np.copy(x)
+
+    def f_neg1(x: np.array) -> np.array: return support_f_1(x + 1) - 1
+
+    result = np.empty(img_window.shape)
+    dict_flip = {-1: f_neg1, 0: f_0, 1: f_1}
     for i in [-1, 0, 1]:
-        temp_indices_x, temp_indices_y = np.where((np_mask == i) == True)
-        np_result[temp_indices_x, temp_indices_y] = dict_flip[i](np_img_window[temp_indices_x, temp_indices_y])
-    return np_result
+        temp_indices_x, temp_indices_y = np.where(mask == i)
+        result[temp_indices_x, temp_indices_y] = dict_flip[i](img_window[temp_indices_x, temp_indices_y])
+    return result
 
 
-def calculate_count_groups(np_img, np_mask):
+def calculate_count_groups(img: np.array, mask: np.array) -> tuple:
+    """
+    :param img: input image
+    :param mask: mask using which the flipping is done
+    :return: tuple of Rm/R-m and Sm/S-m groups are calculated
+    Function divides the image into windows of size of mask. Flips it according to the
+    window passed checks whether the windows are Regular or Singular based on
+    discriminant(flipped)>discriminant(original) --> Regular group else singular
+    """
     count_reg, count_sing, count_unusable = 0, 0, 0
 
-    for ih in range(0, np_img.shape[0], np_mask.shape[0]):
-        for iw in range(0, np_img.shape[1], np_mask.shape[1]):
+    for ih in range(0, img.shape[0], mask.shape[0]):
+        for iw in range(0, img.shape[1], mask.shape[1]):
+            img_window = img[ih: ih + mask.shape[0], iw: iw + mask.shape[1]]  # this is one group
+            flipped_output = flipping_operation(img_window, mask)
 
-            np_img_window = np_img[ih: ih+np_mask.shape[0], iw: iw+np_mask.shape[1]]    # this is one group
-            
-            flipped_output = flipping_operation(np_img_window, np_mask)
-
-            # comparison = flipped_output == np_img_window 
-            # equal_arrays = comparison.all() 
-            # print(equal_arrays)
-
-            discrimination_img_window = discrimination_function(np_img_window)
+            discrimination_img_window = discrimination_function(img_window)
             discrimination_flipped_output = discrimination_function(flipped_output)
 
-            if discrimination_flipped_output > discrimination_img_window: 
+            if discrimination_flipped_output > discrimination_img_window:
                 count_reg += 1
             elif discrimination_flipped_output < discrimination_img_window:
                 count_sing += 1
             else:
                 count_unusable += 1
 
-    totalGroups = count_reg + count_sing + count_unusable
-    return count_reg/totalGroups, count_sing/totalGroups
+    total_groups = (count_reg + count_sing + count_unusable)  # for calculation in scale of 0-1
+    return count_reg / total_groups, count_sing / total_groups
 
 
-def randomString(n):
+def random_string(n: int) -> str:
+    """
+    :param n: size of string to be generated
+    :return: returns string with random content
+    """
     chars = string.ascii_lowercase
     return ''.join(random.choice(chars) for _ in range(n))
 
 
-def simpleLSBFlipper(image, n):
-    flattenedImg = image.flatten()
-    flipped = flattenedImg[0:n]^1       # 0 XOR 1 = 1 ; 1 XOR 1 = 0
-    flattenedImg[0:n] = flipped
-    return flattenedImg.reshape(image.shape)
+def scattered_lsb_flipping(img: np.array, percent: float) -> np.array:
+    """
+    :param img:
+    :param percent: percentage of lsb flips need to be done
+    :return: resultant image after flipping
+    """
+    result = np.copy(img)
+    no_pixels_to_change = int(np.round(percent * np.prod(img.shape)))
+    random_indices_c = np.random.randint(low=0, high=img.shape[1], size=no_pixels_to_change)
+    random_indices_r = np.random.randint(low=0, high=img.shape[0], size=no_pixels_to_change)
+    result[random_indices_r, random_indices_c] = np.bitwise_xor(result[random_indices_r, random_indices_c], 1)
+    return result
 
 
-def scattered_lsb_flipping(np_img, percent):
+def find_root(p_by_2_flipped: np.array, one_p_by_2_flipped: np.array):
+    """
+    :param p_by_2_flipped: Rm, Sm, R-m, S-m values at flipped at p/2 pixels
+    :param one_p_by_2_flipped: Rm, Sm, R-m, S-m values at flipped at all pixels
+    :return: returns required root
+    """
+    rm_p_by_2, sm_p_by_2, r_neg_p_by_2, s_neg_p_by_2 = p_by_2_flipped
+    rm_1_p_by_2, sm_1_p_by_2, r_neg_1_p_by_2, s_neg_1_p_by_2 = one_p_by_2_flipped
 
-    np_result = np.copy(np_img)
-
-    if percent == 1:
-        return np.bitwise_xor(np_result, 1)
-    else:
-        no_pixels_to_change = int(np.round(percent * np.prod(np_img.shape)))
-
-        random_indices_c = np.random.randint(low = 0, high = np_img.shape[1], size = no_pixels_to_change)       # This is like sampling WITH replacement, we need WITHOUT
-        random_indices_r = np.random.randint(low = 0, high = np_img.shape[0], size = no_pixels_to_change)
-
-        # random_indices_c = np.random.choice()
-        # random_indices_r = np.random.choice()
-
-        np_result[random_indices_r, random_indices_c] = np.bitwise_xor(np_result[random_indices_r, random_indices_c], 1)
-        return np_result
-
-
-def RS_quadratic_solver(half_LSBs_flipped, all_LSBs_flipped):
-    
-    RM_p2 = half_LSBs_flipped[0]
-    SM_p2 = half_LSBs_flipped[1]
-    R_minus_M_p2 = half_LSBs_flipped[2]
-    S_minus_M_p2 = half_LSBs_flipped[3]
-
-    RM_1_minus_p2 = all_LSBs_flipped[0]
-    SM_1_minus_p2 = all_LSBs_flipped[1]
-    R_minus_M_1_minus_p2 = all_LSBs_flipped[2]
-    S_minus_M_1_minus_p2 = all_LSBs_flipped[3]
-
-    d0        = RM_p2                -  SM_p2
-    d1        = RM_1_minus_p2        -  SM_1_minus_p2
-    d_minus_0 = R_minus_M_p2         -  S_minus_M_p2
-    d_minus_1 = R_minus_M_1_minus_p2 -  S_minus_M_1_minus_p2
+    d0 = rm_p_by_2 - sm_p_by_2
+    d1 = rm_1_p_by_2 - sm_1_p_by_2
+    d_neg_0 = r_neg_p_by_2 - s_neg_p_by_2
+    d_neg_1 = r_neg_1_p_by_2 - s_neg_1_p_by_2
 
     a = 2 * (d1 + d0)
-    b = (d_minus_0 - d_minus_1 - d1 - 3*d0)
-    c = (d0 - d_minus_0)
+    b = (d_neg_0 - d_neg_1 - d1 - 3 * d0)
+    c = (d0 - d_neg_0)
 
-    coefficients = [a, b, c]
+    roots = np.roots([a, b, c])
+    
+    if len(roots) == 1:
+        return roots[0]
+    else:
+        return roots[1] if abs(roots[0]) > abs(roots[1]) else roots[0]
 
-    return np.roots(coefficients)
 
-def rs_helper(channels, mask, flip = False, percent = 0):
+def rs_helper(channels: list, mask: np.array, flip: bool = False, percent: int = 0) -> np.array:
+    """
+    :param channels: Image channels
+    :param mask: Mask for flipping
+    :param flip: If flag is on then lsb_flipping is done else it is assumed the image is already encoded
+    :param percent: percentage to flip
+    :return: list of Rm, Sm, R-m, S-m values
+    """
     rm, sm, r_neg_m, s_neg_m = 0, 0, 0, 0
     for channel in channels:
         if flip:
